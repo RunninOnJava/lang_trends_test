e4eeb47abbfe052450e94d6859d36e9b0028bbaf
Jordan Freeman <freemanjor08@gmail.com>
2015-07-01 15:49:37 +0000

    fullyWorking

--- app/controllers/do_work_controller.rb
+++ app/controllers/do_work_controller.rb
@@ -1,24 +1,30 @@
 class DoWorkController < ApplicationController
   include ActionView::Helpers::NumberHelper
   def new
-    #@locations = ["Boston","San Fransciso","Los Angeles", "Denver", "Boulder","Chicago","New York"]
-    @locations = ["New York", "Boston", "Denver"]
+    @locations = ["Boston","San Francisco","Los Angeles", "Denver", "Boulder","Chicago","New York"]
+    #@locations = ["New York", "Boston", "Denver", "Los Angeles","Chicago","Boulder"]
     #@langs = ["ruby","python","C++","C","Node","Scala"]
-    @langs= ["Node", "python"]
+    @langs= ["Node", "python","C++","C","Scala","ruby"]
     @jobs = nil
     tmp_hsh = {}
-    @hsh = {}
+    @hsh = Hash.new
+    @final = Hash.new
     @locations.each do |loc|
       total = 0
+      @hsh ={}
+      
       @langs.each do |lang|
         @jobs = Github::Jobs.positions(search: "#{loc}, #{lang}")
         tmp_hsh.merge!("#{lang}" => @jobs.length)
         total += @jobs.length
       end
       tmp_hsh.each do |key, value|
-        percent = (value.to_f / total.to_f) * 100
+        if total == 0 then percent = 0 
+          else percent = (value.to_f / total.to_f) * 100
+        end
         @hsh.merge!("#{key}" => number_to_percentage(percent, precision: 0))
-       #@hsh.merge!("#{key}" => number_to_percentage(value, precision: 0))
+       @final.merge!("#{loc}" => @hsh)
+       
       end
     end
   end
